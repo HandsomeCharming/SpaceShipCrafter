2944954eeb70c2c05e7fe533bbae683769b18330
HandsomeCharming <jerry111zhang@163.com>
2014-11-17 21:57:54 -0500

    add GUIS

---
+++ GUIs.cpp
@@ -0,0 +1,9 @@
+//
+//  GUIs.cpp
+//  FinalProject
+//
+//  Created by  帅哥 on 11/17/14.
+//  Copyright (c) 2014 帅哥. All rights reserved.
+//
+
+#include "GUIs.h"
---
+++ GUIs.h
@@ -0,0 +1,14 @@
+//
+//  GUIs.h
+//  FinalProject
+//
+//  Created by  帅哥 on 11/17/14.
+//  Copyright (c) 2014 帅哥. All rights reserved.
+//
+
+#ifndef __FinalProject__GUIs__
+#define __FinalProject__GUIs__
+
+#include <stdio.h>
+
+#endif /* defined(__FinalProject__GUIs__) */
